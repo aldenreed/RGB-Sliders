52f2066359bb9e0e68ba3a3822e4857adcbd2aa6
asr2230 <asr2230@cs.rit.edu>
2017-06-26 15:12:14 -0400

    The sliders now change the color of the background.

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func updateColor(){
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    @IBAction func redChanged(_ sender: UISlider) {
+        updateColor()
+    }
+    @IBAction func greenChanged(_ sender: UISlider) {
+        updateColor()
+    }
+    @IBAction func blueChanged(_ sender: UISlider) {
+        updateColor()
+    }
 
 }
-
