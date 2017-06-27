d38d3651a5440f65c0bd01222c975f5c117bd3e7
asr2230 <asr2230@cs.rit.edu>
2017-06-27 14:00:05 -0400

    Made the default values change when the color is changed.

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let defaults = UserDefaults.standard
+        redSlider.value = defaults.float(forKey: "red")
+        greenSlider.value = defaults.float(forKey: "green")
+        blueSlider.value = defaults.float(forKey: "blue")
         updateColor()
         colorSquare.layer.borderColor = UIColor.black.cgColor
         colorSquare.layer.borderWidth = 1
@@ -32,6 +36,11 @@ class ViewController: UIViewController {
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
         colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        let defaults = UserDefaults.standard
+        defaults.set(redSlider.value, forKey: "red")
+        defaults.set(greenSlider.value, forKey: "green")
+        defaults.set(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
     }
     @IBAction func redChanged(_ sender: UISlider) {
         updateColor()
@@ -41,6 +50,12 @@ class ViewController: UIViewController {
     }
     @IBAction func blueChanged(_ sender: UISlider) {
         updateColor()
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "openColor"){
+            let newViewController = segue.destination
+            newViewController.view.backgroundColor = colorSquare.backgroundColor
+        }
     }
 
 }
